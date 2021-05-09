3e788f114bdf6a165e56ba45aa0f6d28a9297441
Daniel <danielselareid40@gmail.com>
2021-05-09 15:32:21 +1200

    should update inv on item use

--- res/socket.js
+++ res/socket.js
@@ -55,7 +55,7 @@ function io(io) {
             //use items
             socket.on(`userItem-${Constants.ITEM_WALL}`, (positionGiven) => {
               Saves.Players.getPlayer(playerId)[`useItem${Constants.ITEM_WALL}`](positionGiven);
-              socket.on('getUserList', () => socket.emit('userList', Saves.Players.getActivePlayersStripped(playerId)));
+              socket.emit('userList', Saves.Players.getActivePlayersStripped(playerId));
             });
 
             // socket.on('userShoot', () => {
